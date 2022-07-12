49f9114bdb2c5b5e6849e127106737d089981cb9
Steve Reed <stevecreed@gmail.com>
2022-07-12 09:54:57 -0700

    Adjsuted the email message

--- app/Http/Controllers/AlumniController.php
+++ app/Http/Controllers/AlumniController.php
@@ -40,7 +40,7 @@ class AlumniController extends Controller
         //
 
         $messages = [
-            'alumniEmail' => 'The alumni email has already been taken. If you wish to print another membership card, navigate to the <a href="/alumni-card/retrieve">Retrieve Card page.</a>',
+            'alumniEmail.required' => 'The alumni email has already been taken. If you wish to print another membership card, navigate to the <a href="/alumni-card/retrieve">Retrieve Card page.</a>',
         ];
 
         $validator = Validator::make($request->all(), [
