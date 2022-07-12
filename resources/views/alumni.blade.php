774f8713a20af99d733c3f119676163d1397d803
Steve Reed <stevecreed@gmail.com>
2022-07-12 10:35:55 -0700

    Escape the error

--- resources/views/alumni.blade.php
+++ resources/views/alumni.blade.php
@@ -28,7 +28,7 @@ $familyMembers = config('global.familyMembers');
             <div class="alert alert-danger ">
                 <ul>
                     @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
+                        <li>@{{ $error }}</li>
                     @endforeach
                 </ul>
             </div>
