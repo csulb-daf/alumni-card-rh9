8b7c42c4295f27baf408fb78dfba706975f0bc60
Steve Reed <stevecreed@gmail.com>
2022-10-31 15:12:13 -0700

    change file name

--- app/Http/Controllers/AlumniController.php
+++ app/Http/Controllers/AlumniController.php
@@ -207,8 +207,8 @@ class AlumniController extends Controller
 
         //attachment
         $body .= "--$boundary\r\n";
-        $body .="Content-Type: image/jpeg; name="."test_attachment.jpg"."\r\n";
-        $body .="Content-Disposition: attachment; filename="."test_attachment.jpg"."\r\n";
+        $body .="Content-Type: image/jpeg; name=".$fileName."\r\n";
+        $body .="Content-Disposition: attachment; filename=".$fileName."\r\n";
         $body .="Content-Transfer-Encoding: base64\r\n";
         $body .="X-Attachment-Id: ".rand(1000, 99999)."\r\n\r\n";
         $body .= $encoded_content; // Attaching the encoded file with email
