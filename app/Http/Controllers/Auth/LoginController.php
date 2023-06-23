7ac2931e891ea795cbab1d417165134fba4da61d
Steve Reed <stevecreed@gmail.com>
2023-06-23 10:19:57 -0700

    Add ldap login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\MessageBag;
+
+
+use App\Providers\RouteServiceProvider;
+use Symfony\Component\Console\Input\Input;
+
 
 class LoginController extends Controller
 {
@@ -36,5 +45,58 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email','password');
+
+        $success = $this->verify($credentials['email'],$credentials['password']);
+
+        if($success)
+        {
+            $eml = $credentials['email'];
+            $user = User::where('email', '=', $eml)->first();
+
+            if ($user) {
+                Auth::login($user);
+                return redirect('/');
+            }
+
+            else {
+                session()->flash('msg', 'You are not authorized to access this application.');
+                return redirect()->back();
+                //return Redirect::back()->withErrors(['msg', 'You are not authorized to access this application.']);
+            }
+        }
+        else {
+            //$errors = new MessageBag(['password' => ['Email and/or password invalid.']]);
+            $errors = new MessageBag;
+            $errors->add ('password', 'Invalid email or password.');
+            $errors->add('email','Invalid email or password');
+            return Redirect::back()->withErrors($errors)->withInput($request->except('password'));
+        }
+    }
+
+    public function verify($eml, $pwd)
+    {
+        $host = env('ADLDS_HOST');
+        $ldapport = env('ADLDS_PORT');
+        $connection = @ldap_connect($host,$ldapport);
+        $success = false;
+
+        if ($connection) {
+            ldap_set_option($connection, LDAP_OPT_PROTOCOL_VERSION, 3);
+            ldap_set_option($connection, LDAP_OPT_REFERRALS, 0);
+
+            $bind = @ldap_bind($connection, $eml, $pwd);
+            if ($bind) {
+                $success = true;
+
+                ldap_close($connection);
+            }
+        }
+        return $success;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,20 +24,6 @@ Route::get('/alumni', function () {
     // ...
 });
 
+Auth::routes(['register' => false]);
 Route::get('/export', 'HomeController@export');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
