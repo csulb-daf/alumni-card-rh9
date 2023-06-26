8949d138977048b030cfa9bc1267e6139dd5647f
Steve Reed <stevecreed@gmail.com>
2023-06-26 15:16:16 -0700

    adjusted the headres

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -66,7 +66,7 @@ class HomeController extends Controller
             'Alumni Created Date',
             'Alumni Updated Link'
             ];
-        return Excel::download(new AlumniExport, 'alumni.xlsx',null, null, $headers);
+        return Excel::download(new AlumniExport, 'alumni.xlsx',null,  $headers);
 
 
     }
