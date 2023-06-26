cbe3d315034e1b0202a1b10866e776408a64ef69
Steve Reed <stevecreed@gmail.com>
2023-06-26 16:41:41 -0700

    adding custom headers

--- app/Exports/AlumniExport.php
+++ app/Exports/AlumniExport.php
@@ -4,11 +4,52 @@ namespace App\Exports;
 use App\Alumni;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class AlumniExport implements FromCollection
+class AlumniExport implements FromCollection, WithHeadings
 {
     public function collection()
     {
         return Alumni::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Alumni ID',
+            'Alumni First Name',
+            'Alumni Middle Initial',
+            'Alumni Last Name',
+            'Alumni Pronouns',
+            'Alumni Name With Attending',
+            'Alumni Email',
+            'Alumni Affiliation',
+            'Alumni Grad Year',
+            'Alumni Degree Type',
+            'Alumni Major',
+            'Alumni Beach Family Member',
+            'Alumni Beach Name',
+            'Alumni Home Phone',
+            'Alumni Cell Phone',
+            'Alumni Street Address One',
+            'Alumni Street Address Two',
+            'Alumni City',
+            'Alumni State',
+            'Alumni Zip',
+            'Alumni Country',
+            'Alumni LinkedIn Profile',
+            'Alumni Facebook Profile',
+            'Alumni Twitter Profile',
+            'Alumni Instagram Profile',
+            'Alumni Membership Token',
+            'Alumni Business Employer',
+            'Alumni Job Title',
+            'Alumni Business Phone Number',
+            'Alumni Business Email',
+            'Alumni Business Address',
+            'Alumni Opportunities',
+            'Alumni Digital Card Link',
+            'Alumni Created Date',
+            'Alumni Updated Link'];
     }
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use App\Exports\AlumniExport;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 
+
 class HomeController extends Controller
 {
     /**
@@ -30,43 +31,9 @@ class HomeController extends Controller
 
     public function export()
     {
-        $headers = ['Alumni ID',
-            'Alumni First Name',
-            'Alumni Middle Initial',
-            'Alumni Last Name',
-            'Alumni Pronouns',
-            'Alumni Name With Attending',
-            'Alumni Email',
-            'Alumni Affiliation',
-            'Alumni Grad Year',
-            'Alumni Degree Type',
-            'Alumni Major',
-            'Alumni Beach Family Member',
-            'Alumni Beach Name',
-            'Alumni Home Phone',
-            'Alumni Cell Phone',
-            'Alumni Street Address One',
-            'Alumni Street Address Two',
-            'Alumni City',
-            'Alumni State',
-            'Alumni Zip',
-            'Alumni Country',
-            'Alumni LinkedIn Profile',
-            'Alumni Facebook Profile',
-            'Alumni Twitter Profile',
-            'Alumni Instagram Profile',
-            'Alumni Membership Token',
-            'Alumni Business Employer',
-            'Alumni Job Title',
-            'Alumni Business Phone Number',
-            'Alumni Business Email',
-            'Alumni Business Address',
-            'Alumni Opportunities',
-            'Alumni Digital Card Link',
-            'Alumni Created Date',
-            'Alumni Updated Link'
-            ];
-        return Excel::download(new AlumniExport, 'alumni.xlsx',null,  $headers);
+        $dataTimestamp = date('m/d/Y');
+
+        return Excel::download(new AlumniExport, 'alumni export ' . $dataTimestamp .'.xlsx');
 
 
     }
